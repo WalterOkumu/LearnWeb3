0d841f7f5dff67155c917c6bd6fdfa62e878df2b
Okumu Oriaro <okumu.oriaro@gmail.com>
2022-06-19 18:25:33 +0300

    04. DAO done

---
+++ 04. DAO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xc28b665B3BF78d76Bd83c8EC271CcB7D4E2b991F";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
