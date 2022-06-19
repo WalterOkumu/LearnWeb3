ee8b489552ee3fae1827a8f1afd1e24919868ce7
Okumu Oriaro <okumu.oriaro@gmail.com>
2022-06-19 17:29:51 +0300

    03. Initial Coin Offering done

---
+++ 03. Initial Coin Offering/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x43d45F8928F2A453e494E7Bdc8518fA104C9b87C";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
